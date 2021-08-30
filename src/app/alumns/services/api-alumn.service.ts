1ceb6b383c0a76044eb4fbef79f120b5f1685e3b
Daniel Meza <danielmeza.4c@gmail.com>
2021-08-29 23:46:04 -0700

    Servicio para comunicar con API

---
+++ src/app/alumns/services/api-alumn.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiResponse } from 'src/app/models/api-response.model';
+import { environment } from 'src/environments/environment';
+import { Alumn } from '../alumn.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiAlumnService {
+
+  private _apiURL = environment.apiUrl + '/Alumns';
+
+  constructor(private _http: HttpClient) { }
+
+  getAllAlumns(): Observable<ApiResponse<Alumn[]>> {
+    return this._http.get<ApiResponse<Alumn[]>>(this._apiURL);
+  }
+
+  getAlumnById(alumnId: number): Observable<ApiResponse<Alumn>> {
+    return this._http.get<ApiResponse<Alumn>>(`${this._apiURL}/${alumnId}`);
+  }
+
+  addAlumn(alumn: Alumn): Observable<ApiResponse<Alumn>> {
+    return this._http.post<ApiResponse<Alumn>>(this._apiURL, alumn);
+  }
+
+}
